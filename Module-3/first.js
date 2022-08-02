9635694636afa816c6f9e1d39007b1aca882994f
jyosuraj <jyosurajhs@gmail.com>
2022-08-02 13:08:52 +0530

    Module 3 JavaScript LetsUpgrade

---
+++ Module-3/first.js
@@ -0,0 +1,74 @@
+// console.log("I am External JavaScript")
+/* let firstName = "Suraj";
+let lastName = "HS";
+
+console.log(`First Name ${firstName}\nLastName ${lastName}`);
+console.log(`Full Name ${firstName} ${lastName}`); */
+
+
+// Condition
+// conditional operators: <, >, <=, >=, !=, ==.
+// logical operators: &&, ||, !.
+
+/* let num = 45;
+
+if(num < 3)
+{
+    console.log("True Statement.");
+}
+else
+{
+    console.log("False Statement.");
+}
+
+console.log("General Code"); */
+
+
+// switch statement: Is a type of SELECTION CONTROL MECHANISM
+        // -> switch is faster than if condition 
+
+// let choice = 1;
+
+// switch(choice)
+// {
+//     case 1:
+//         {
+//             console.log("Case 01.\n");
+//         }
+//         // break;
+//         /* if we don't put break keyword then case 2 & following statements are executed => FALL OUT || BREAK THROUGH */
+
+//     case 2:
+//         {
+//             console.log("Case 02.\n");
+//         }
+//         break;
+//     default:
+//         {
+//             console.log("Default Statement.\n");
+//         }
+// }
+
+// loops: while, do while, for.
+
+// let => block scope
+// var => function scope
+// -> let keyword is upgrade of var
+
+
+let num = 2;
+
+if(num <= 10)
+{
+    let x = 10; // x is not defined
+    var x = 10; // x = 10
+}
+console.log(x);
+
+
+
+
+
+
+        
+ 
