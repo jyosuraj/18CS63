3692afcd48aa634c2109878b0b88ff4d630a3c16
jyosuraj <jyosurajhs@gmail.com>
2022-08-06 10:15:31 +0530

    DOM in JavaScript

---
+++ Module-3/dom.js
@@ -0,0 +1,21 @@
+// through tagname
+// document.querySelector("h1").innerText="Changed Content.";
+
+// through id
+// document.querySelector("#para").innerText="This changed paragraph";
+ 
+// document.getElementById("para").innerText="Suman Suraj Para Id";
+
+// console.log(document.querySelectorAll("h1")[0].innerHTML)
+
+// document.querySelectorAll("h1")[1].style.background="green";
+// document.querySelectorAll("h1")[1].style.fontSize="50px";
+
+// Event Handling.
+
+function changeBgColor()
+{
+    document.querySelectorAll("h1")[1].style.background="green";
+}
+
+
